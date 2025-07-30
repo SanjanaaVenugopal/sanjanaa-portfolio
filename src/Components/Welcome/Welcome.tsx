d18ffaa07de51720c1e9ce1427eb107304edf796
sanjanaa <snjoo123@gmail.com>
2025-07-30 14:29:38 +0530

    Profile pic

--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { welcome } from "../../Constants/constants";
+import { welcome } from "../../constants/constants";
+import ProfilePic from "../../constants/ProfilePic.jpg"
 
 type props = {};
 
 export const Welcome: React.FC<props> = () => {
   return (
-      <div className="p-10 text-center">
-        <h1 className="text-5xl font-bold">{welcome.headline}</h1>
-        <p className="text-lg mt-2 text-muted-foreground">{welcome.tagline}</p>
-      </div>
+    <div className="p-10 flex items-center gap-4 align-center">
+        <img
+            src={ProfilePic}
+            alt={welcome.dpAltText}
+            className="w-64 h-64 rounded-full object-cover border-2 border-white shadow-md"
+        />
+        <div>
+            <h1 className="text-5xl font-bold">{welcome.headline}</h1>
+            <p className="text-lg mt-2 text-muted-foreground">{welcome.tagline}</p>
+        </div>
+    </div>
   );
 }
--- src/Constants/constants.ts
+++ src/Constants/constants.ts
@@ -1,4 +1,5 @@
 export enum welcome {
     headline = "Hi, I'm Sanjanaa 👋",
-    tagline = "Welcome to my whimsical corner of the web" 
+    tagline = "Welcome to my whimsical corner of the web",
+    dpAltText = "Sanjanaa's profile" 
 }
